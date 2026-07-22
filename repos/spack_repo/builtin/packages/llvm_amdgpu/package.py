5059aba8cd17b4097dae1e0e45ea77bc698c4341
renjithravindrankannath <rravindr@amd.com>
2026-07-22 16:57:21 -0400

    Update 7.13 to Support air-gapped build

--- repos/spack_repo/builtin/packages/llvm_amdgpu/package.py
+++ repos/spack_repo/builtin/packages/llvm_amdgpu/package.py
@@ -274,6 +274,7 @@ class LlvmAmdgpu(CMakePackage, LlvmDetection, CompilerPackage):
         ("7.2.0", "9e3836d7d6023843a72ecd3fbf3f09b1b6747a9e"),
         ("7.2.1", "9e3836d7d6023843a72ecd3fbf3f09b1b6747a9e"),
         ("7.2.3", "9e3836d7d6023843a72ecd3fbf3f09b1b6747a9e"),
+        ("7.13.0", "9e3836d7d6023843a72ecd3fbf3f09b1b6747a9e"),
     ]:
         resource(
             name="spirv-headers",

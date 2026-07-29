e3ed467a6593fd743a87325ecbec9e9eebce14ac
renjithravindrankannath <rravindr@amd.com>
2026-07-29 02:39:25 -0400

    adding commit hash for migraphx 7.13

--- repos/spack_repo/builtin/packages/migraphx/package.py
+++ repos/spack_repo/builtin/packages/migraphx/package.py
@@ -25,7 +25,9 @@ class Migraphx(ROCmLibrary, CMakePackage):
     rocm_url_map = [(None, "https://github.com/ROCm/AMDMIGraphX/archive/rocm-{0}.tar.gz")]
 
     license("MIT")
-    version("7.13.0", branch="release/rocm-rel-7.13")
+    version(
+        "7.13.0", branch="release/rocm-rel-7.13", commit="f066712b04f87e927217edee3fc630e856787eb2"
+    )
     version("7.2.3", sha256="25d491d83fe84c6c071305a71100f77b393b35ae5c9eeec277c68986378f6abc")
     version("7.2.1", sha256="611e4646f11fe559946275a6c79f1aaabe0a5c7cb95a42b28e724ba29f4c753a")
     version("7.2.0", sha256="085ea6fcf6197b20fed60917194ca622e5d2c1705237fe063563f988494a8b3d")

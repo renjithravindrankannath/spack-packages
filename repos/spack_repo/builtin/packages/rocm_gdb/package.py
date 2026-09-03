d741b887320bc5be592cc3fa336025c6c3cef86a
renjithravindrankannath <rravindr@amd.com>
2026-09-03 06:09:23 -0400

    Updating rocm-wqgdb

--- repos/spack_repo/builtin/packages/rocm_gdb/package.py
+++ repos/spack_repo/builtin/packages/rocm_gdb/package.py
@@ -25,7 +25,7 @@ class RocmGdb(ROCmLibrary, AutotoolsPackage):
     ]
     maintainers("srekolam", "renjithravindrankannath")
 
-    version("10.0.0", sha256="f30517ed6d9e18cde104eb487f173e62fed0175083a9498ca383f8136a9f4eec")
+    version("10.0.0", sha256="82a2d623c3d17e8ee14ef0d668fa6ef870542ae14b6ff8cbab3ed01286ffdffd")
     version("7.14.0", sha256="60d2e2a9e0af3b42e52f57c9dddbc34033933cb813d4da081fbd41af2c3c57c2")
     version("7.13.0", sha256="979533cc1a207e8a65755224bdc407e443144c40200fd22b324d1a986a646ad1")
     version("7.2.3", sha256="ce7e26f5470ed7afa4cb842d562e4ae6778f0ce123eed81ce10d867eb3ec0d80")

b4a91a440617e17e77359c1acd0724e0fdc0aa98
renjithravindrankannath <rravindr@amd.com>
2026-09-15 13:53:01 -0400

    Style error fix

--- repos/spack_repo/builtin/packages/rocprofiler_systems/package.py
+++ repos/spack_repo/builtin/packages/rocprofiler_systems/package.py
@@ -403,7 +403,7 @@ class RocprofilerSystems(ROCmLibrary, CMakePackage):
         return args
 
     def flag_handler(self, name, flags):
-        if (self.spec.satisfies("@6.3:7.1") or self.spec.satisfies("@7.14:")):
+        if self.spec.satisfies("@6.3:7.1") or self.spec.satisfies("@7.14:"):
             if name == "ldflags":
                 flags.append("-lintl")
         return (flags, None, None)
